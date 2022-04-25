42cb0cfa2f84c26e6fd804dfcd8a8e52f13f067d
ChristianAtmaluhur <2111500002@mahasiswa.atmaluhur.ac.id>
2022-04-25 11:58:31 +0700

    Menyelesaikan fitur tambahan

---
+++ index.js
@@ -0,0 +1,56 @@
+const requested = []
+
+const menuBtn = document.querySelectorAll('.menu button')
+const listContainer = document.querySelector('.task .list-container')
+const total = document.querySelector('.total .right span')
+const sendBtn = document.querySelector('.send-btn')
+
+menuBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        requested.push(btn.value)
+        updateArray(btn)
+    })
+});
+
+sendBtn.addEventListener('click', () => {
+    requested.splice(0, requested.length)
+    updateArray()
+})
+
+function updateArray() {
+    let listsDom = ''
+    let price = 0
+    let sum = 0
+    const filterArray = uniq(requested)
+
+    for(let i = 0; i < filterArray.length; i++) {
+
+        if(filterArray[i] == 'Wash Car') {
+            price = 10
+        } else if(filterArray[i] == 'Mow Lawn') {
+            price = 20
+        } else {
+            price = 30
+        }
+
+        sum += price
+
+        listsDom += `
+            <div class="list">
+                <div class="left">
+                    <h1>${filterArray[i]}</h1>
+                    <button>remove</button>
+                </div>
+                <p><span>$</span>${price}</p>
+            </div>
+        `
+    }
+    total.textContent = sum
+    listContainer.innerHTML = listsDom
+}
+
+function uniq(requested) {
+    return Array.from(new Set(requested));
+}
+
+
